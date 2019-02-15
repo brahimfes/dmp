cdceecd6a6ad80fdc959653384778a0164ccc565
Brahim YAKOUBI <brahim.yacoubi@cpexterne.org>
2019-02-15 02:50:17 +0100

    add new barre nav bootstrap 4

--- afficheDonnee.php
+++ afficheDonnee.php
@@ -142,19 +142,6 @@ if (isset($_POST['rendezvous'])) {
 				<h1>Rendez-vous</h1>
 				<!-- <button type="button" class="btn btn-success">Nouveau rendez-vous</button> <br /> -->
 
-				<a class="btn btn-primary" role="button" data-toggle="collapse" href="#collapseExample" aria-expanded="false"
-				 aria-controls="collapseExample">
-					Link with href
-				</a>
-				<button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false"
-				 aria-controls="collapseExample">
-					Button with data-target
-				</button>
-				<div class="collapse" id="collapseExample">
-					<div class="well">
-						...
-					</div>
-				</div>
 				<p>
 					<button class="btn btn-success" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false"
 					 aria-controls="collapseExample">
---
+++ barre_de_nav.1.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<a class="navbar-brand" href="#">MES</a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+	 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav navbar-right">
+			<li class="nav-item active">
+				<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+			</li>
+			<li class="nav-item"><a href="accueil.php">Accueil</a></li>
+			<li class="nav-item"><a href="maintenance.php">Maintenance</a></li>
+			<li class="nav-item"><a href="analyse.php">Donnee collectee</a></li>
+			<li class="nav-item"><a href="patient.php">Patient</a></li>
+			<li class="nav-item"><a href="modifier.php">Compte</a></li>
+			<li class="nav-item"><a href="deconnexion.php">Deconnexion</a></li>
+
+		</ul>
+	</div>
+</nav>
