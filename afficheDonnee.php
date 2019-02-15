fcf6002f3c66a4acb700b1fde79ba8083c5d59a8
Brahim YAKOUBI <brahim.yacoubi@cpexterne.org>
2019-02-15 01:30:43 +0100

    add menu on patient page

--- afficheDonnee.php
+++ afficheDonnee.php
@@ -1,21 +1,14 @@
 <?php
 session_start();
-include_once 'includes.php';
-include ("barre_de_nav.php");
+include_once('includes.php');
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
 
 <head>
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-	 crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
-	 crossorigin="anonymous"></script>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
-	 crossorigin="anonymous">
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
-	 crossorigin="anonymous"></script>
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+	<link href="bootstrap/js/bootstrap.js" rel="stylesheet" type="text/css"/>
 	 <script src="js/parser.js"></script>
 	<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
 	<title>affichage donnee patient</title>
@@ -23,6 +16,7 @@ include ("barre_de_nav.php");
 
 <body>
 	<?php
+include "barre_de_nav.php";
 
 $errors = array();
 $patient;
